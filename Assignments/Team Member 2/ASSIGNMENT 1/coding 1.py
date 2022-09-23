b55a904cdc01ac3ba91c78eee880b169f09bb08f
Dharsinis11 <82928240+Dharsinis11@users.noreply.github.com>
2022-09-23 15:23:12 +0530

    Add files via upload

---
+++ Assignments/Team Member 2/ASSIGNMENT 1/coding 1.py
@@ -0,0 +1,54 @@
+
+
+a=[1,2,3,4,5]
+print(a)
+
+#insertion
+a.insert(5,10)
+print(a)
+
+#deletion of first occurence of integer
+del a[0]
+print(a)
+
+#insertion at end of a
+a.append(-5)
+print(a)
+
+#sorting
+a.sort()
+print(a)
+
+#popping last element
+a.pop()
+print(a)
+
+#reverse the list
+a.reverse()
+print(a)
+
+#STRING OPERATIONS:
+
+#concatenate string
+s1=("Hi")
+s2=("There")
+print(s1+s2)
+
+#reverse of String
+s3="Welcome"
+print(s3[::-1])
+
+#slice
+print(s3[slice(1,5,2)])
+
+
+
+
+
+
+
+
+
+
+
+
