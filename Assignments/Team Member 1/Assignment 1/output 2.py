6edfaefd99621820654651a7de6ea3481e75ad26
MNaveena26 <112945260+MNaveena26@users.noreply.github.com>
2022-09-23 15:17:32 +0530

    Add files via upload

---
+++ Assignments/Team Member 1/Assignment 1/output 2.py
@@ -0,0 +1,14 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================================================================================ RESTART: D:/coding 2.py ===============================================================================
+please select operation -
+1. Add
+2. Subtract
+3. Multiply
+4. Divide
+
+select operations from 1, 2, 3, 4 :4
+Enter first number: 13567
+Enter second number: 1234
+13567 / 1234 = 10.994327390599675
